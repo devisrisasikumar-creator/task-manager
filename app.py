f00dc52556a5627f74c6a760df125c4d85c63d6a
Devishree <psdevishree@gmail.com>
2026-05-17 21:29:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+tasks=[]
+
+@app.route('/',methods=['GET','POST'])
+def home():
+
+    if request.method=='POST':
+
+        task=request.form.get('task')
+
+        tasks.append(task)
+
+    return render_template('dashboard.html',tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
